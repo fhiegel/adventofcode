5c82369f803263fe91a5bc68d0cc006952e8851d
Fabien HIEGEL <fhiegel@maisonsdumonde.com>
2022-12-02 13:38:06 +0100

    :sparkles: Solves day1.1

--- src/2022/day01/day01.test.js
+++ src/2022/day01/day01.test.js
@@ -16,6 +16,13 @@ function readFileAsLines(fileName) {
     });
 }
 
+function readFileAsString(fileName) {
+    const filePath = path.join(__dirname, fileName)
+    return fs.readFileSync(filePath, {
+        encoding: "utf8"
+    });
+}
+
 async function computeCalories(lines) {
     let sum = 0;
     for await (const line of lines) {
@@ -25,16 +32,55 @@ async function computeCalories(lines) {
     return sum;
 }
 
+function mapInventoriesToCalories(fileAsString) {
+    let lines = fileAsString.split('\n');
+
+    let inventoryIndex = 0;
+    return lines
+        .reduce((inventories, item, index) => {
+            if (0 < item.length) {
+                inventories[inventoryIndex].push(parseInt(item))
+            } else {
+                inventoryIndex++
+                inventories[inventoryIndex] = []
+            }
+            return inventories
+        }, [[]])
+        .filter(inventory => 0 < inventory.length)
+        .map(inventory => inventory.reduce((total, current) => total + current), 0);
+}
+
+function computeInventoriesMaxCalories(fileAsString) {
+    const lines = mapInventoriesToCalories(fileAsString)
+    return Math.max(...lines);
+}
+
 describe('Day 1: Calorie Counting. ', () => {
 
     describe('#1 How many calories carries the Elf with the most calories', () => {
 
         it('get sum of elf inventory', async () => {
-            const lines = readFileAsLines('./get_sum');
+            const lines = readFileAsString('./get_sum');
 
-            const sum = await computeCalories(lines);
+            const max = computeInventoriesMaxCalories(lines);
 
-            expect(sum).toBe(100)
+            expect(max).toBe(100)
+        })
+
+        it('get max of elves inventories', async () => {
+            const fileAsString = readFileAsString('./get_max');
+
+            const max = computeInventoriesMaxCalories(fileAsString)
+
+            expect(max).toBe(40)
+        })
+
+        it('get answer to my puzzle', async () => {
+            const fileAsString = readFileAsString('./input');
+
+            const max = computeInventoriesMaxCalories(fileAsString)
+
+            expect(max).toBe(70613)
         })
 
     })
